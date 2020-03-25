5eb931163357d83c7c15a0a24f9a04fffcef7c8c
Kamil <kamil55525@gmail.com>
2020-03-25 19:35:14 +0100

    Add reading input

--- list1/task2.py
+++ list1/task2.py
@@ -1,11 +1,20 @@
-from collections import defaultdict
 
 
 def read_input(filename: str):
-    cities = defaultdict(list)
+    cities = list()
     print(cities)
     with open(filename, 'r') as file:
-        first_line = file.readline(limit=1)
-        time, cities_count = ((str(x) for x in first_line.split()))
-        for i, line in enumerate(file):
-            cities[i].extend(int(x) for x in line.split())
+        first_line = file.readline()
+        print([str(x) for x in first_line.split()])
+        [time, cities_count] = [int(x) for x in first_line.split()]
+        for i, line in enumerate(file, 0):
+            cities.append([])
+            cities[i] = [int(x)for x in line.split()]
+        if cities_count != len(cities):
+            print(f'Number of read cities is different from declared.')
+            exit(1)
+    print(cities)
+
+
+if __name__ == '__main__':
+    read_input('l1z2a.txt')
