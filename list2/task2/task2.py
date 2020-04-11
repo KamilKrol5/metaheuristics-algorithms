2c237a33070f86d8f575e44e6cf038acd60a22e3
Kamil <kamil55525@gmail.com>
2020-04-12 00:00:23 +0200

    Add mse, skeleton for SA and begin initial solution generation

--- list2/task2/task2.py
+++ list2/task2/task2.py
@@ -1,19 +1,40 @@
 import fileinput
+import time
 from typing import Tuple
 import numpy as np
 from matplotlib import pyplot as plt
 
 
 class ImageApproximationInstance:
-    def __init__(self, matrix, k_coefficient, max_time):
+
+    def __init__(self, matrix, k_coefficient, max_time, target_color_values):
         self.matrix = matrix
+        self.rows, self.columns = matrix.shape
+        # print(self.rows, self.columns)
         self.k = k_coefficient
         self.max_time = max_time
+        self.target_color_values = target_color_values
+        self.middle_points_between_target_values = self.initialize_target_colors()
+
+    def initialize_target_colors(self):
+        return [
+            np.mean(pair)
+            for pair in zip(self.target_color_values, self.target_color_values[1:])]
+
+    def convert_value_to_closest_target_value(self, value):
+        for i, t_val in enumerate(self.middle_points_between_target_values):
+            if value < t_val:
+                return self.target_color_values[i]
+        return self.target_color_values[-1]
+
+    @staticmethod
+    def _get_random_neighbour(solution):
+        pass
 
     @classmethod
-    def from_fileinput(cls):
+    def from_file_input(cls, target_color_values):
         matrix, max_time, k = cls._read_input()
-        return cls(matrix, k, max_time)
+        return cls(matrix, k, max_time, target_color_values)
 
     @staticmethod
     def _read_input() -> Tuple[np.ndarray, int, int]:
@@ -27,12 +48,69 @@ class ImageApproximationInstance:
                     matrix[i][j] = int(x)
         return matrix, max_time, k
 
-    def visualise(self):
-        plt.imshow(self.matrix)
+    @staticmethod
+    def _compute_mse(matrix1, matrix2):
+        if matrix1.shape != matrix2.shape:
+            raise ValueError(f'Matrices does not have the same shape')
+        n, m = matrix1.shape
+        return np.sum((matrix1 - matrix2) ** 2) / (n * m)
+
+    def _compute_mse_of_image_and_other(self, other_matrix):
+        return self._compute_mse(self.matrix, other_matrix)
+
+    @staticmethod
+    def _probability(delta_f, temperature, c):
+        if delta_f <= 0:
+            return 1
+        return 1.0 / (1.0 + np.power(np.e, c * delta_f / temperature))
+
+    def _generate_initial_solution(self):
+        working_matrix = np.copy(self.matrix)
+        k = self.k
+        for i in range(0, self.rows, k):
+            for j in range(0, self.columns, k):
+                mean_value_in_submatrix = np.mean(working_matrix[i:i + k][j:j + k])
+                working_matrix[i:i + k][j:j + k] = \
+                    self.convert_value_to_closest_target_value(mean_value_in_submatrix)
+                # TODO finish it
+        return working_matrix
+
+    def simulated_annealing(self,
+                            initial_temperature,
+                            red_factor,
+                            c=-1):
+        end_time = time.time() + self.max_time
+        temperature = initial_temperature
+
+        current_solution = self._generate_initial_solution()
+        print(current_solution)
+
+        while temperature > red_factor and time.time() < end_time:
+
+            neighbour = self._get_random_neighbour(current_solution)
+
+            delta_f = \
+                self._compute_mse_of_image_and_other(neighbour) - self._compute_mse_of_image_and_other(current_solution)
+
+            if self._probability(delta_f, temperature, c) > np.random.rand():
+                current_solution = neighbour
+            temperature = temperature * (1 - red_factor)
+
+        return current_solution, self._compute_mse_of_image_and_other(current_solution)
+
+    @staticmethod
+    def visualise_matrix(matrix):
+        plt.imshow(matrix)
         plt.gray()
         plt.show()
 
+    def visualise(self):
+        self.visualise_matrix(self.matrix)
+
 
 if __name__ == '__main__':
-    problemInstance = ImageApproximationInstance.from_fileinput()
-    problemInstance.visualise()
+    problemInstance = ImageApproximationInstance.from_file_input([0, 32, 64, 128, 160, 192, 223, 255])
+    # problemInstance.visualise()
+    # print(problemInstance.compute_mse(np.array([[1, 1, 1],
+    #                                       [2, 2, 2]]), np.array([[0, 0, 0],
+    #                                                              [3, 4, 5]])))
