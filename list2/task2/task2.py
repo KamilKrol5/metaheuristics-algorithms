29d2cd66aad2b115062de343b70f5e5ed9824ff3
Kamil <kamil55525@gmail.com>
2020-04-16 22:48:39 +0200

    Improvements

--- list2/task2/task2.py
+++ list2/task2/task2.py
@@ -1,35 +1,52 @@
 import fileinput
 import time
-from typing import Tuple
+from copy import deepcopy
+from dataclasses import make_dataclass
+from typing import Tuple, List
+
 import numpy as np
 from matplotlib import pyplot as plt
+
+_Block = make_dataclass('Block', ['x_start', 'y_start', 'x_length', 'y_length', 'value_inside'])
+
+
+class _Solution:
+    def __init__(self, image_matrix, blocks: List[_Block]):
+        self.blocks: List[_Block] = blocks
+        self.matrix = image_matrix
 
 
 class ImageApproximationInstance:
 
-    def __init__(self, matrix, k_coefficient, max_time, target_color_values):
+    def __init__(self, matrix, k_coefficient, max_time, target_color_values: List[int]):
         self.matrix = matrix
         self.rows, self.columns = matrix.shape
         # print(self.rows, self.columns)
         self.k = k_coefficient
         self.max_time = max_time
-        self.target_color_values = target_color_values
-        self.middle_points_between_target_values = self.initialize_target_colors()
+        self.target_color_values: List[int] = target_color_values
+        self.middle_points_between_target_values = self._initialize_target_colors()
 
-    def initialize_target_colors(self):
+    def _initialize_target_colors(self):
         return [
             np.mean(pair)
             for pair in zip(self.target_color_values, self.target_color_values[1:])]
 
-    def convert_value_to_closest_target_value(self, value):
+    def _convert_value_to_closest_target_value(self, value):
         for i, t_val in enumerate(self.middle_points_between_target_values):
             if value < t_val:
                 return self.target_color_values[i]
         return self.target_color_values[-1]
 
-    @staticmethod
-    def _get_random_neighbour(solution):
-        pass
+    """ Destroys provided solution - it does not copy the matrix not the blocks. 
+    """
+    def _get_random_neighbour(self, solution: _Solution) -> _Solution:
+        for block in solution.blocks:
+            block.value_inside = np.random.choice(self.target_color_values)
+            end_row = block.x_start + block.x_length
+            end_column = block.y_start + block.y_length
+            solution.matrix[block.x_start:end_row, block.y_start:end_column] = block.value_inside
+        return solution
 
     @classmethod
     def from_file_input(cls, target_color_values):
@@ -71,21 +88,24 @@ class ImageApproximationInstance:
         If matrix size is not divisible by K, then blocks at the end (right and bottom) are extended to cover
         the entire matrix.
     """
+
     def _generate_initial_solution(self):
         working_matrix = np.copy(self.matrix)
+        blocks_ = []
         k = self.k
         for i in range(0, self.rows - k + 1, k):
             row_end_range = i + k
             if i + 2 * k > self.rows:
-                row_end_range = i + 2 * k
+                row_end_range = i + k + (self.rows % k)
             for j in range(0, self.columns - k + 1, k):
                 column_end_range = j + k
                 if j + 2 * k > self.columns:
-                    column_end_range = j + 2 * k
+                    column_end_range = j + k + (self.columns % k)
                 mean_value_in_submatrix = np.mean(working_matrix[i:row_end_range, j:column_end_range])
                 working_matrix[i:row_end_range, j:column_end_range] = \
-                    self.convert_value_to_closest_target_value(mean_value_in_submatrix)
-        return working_matrix
+                    self._convert_value_to_closest_target_value(mean_value_in_submatrix)
+                blocks_.append(_Block(i, j, row_end_range - i, column_end_range - j, mean_value_in_submatrix))
+        return _Solution(working_matrix, blocks_)
 
     def simulated_annealing(self,
                             initial_temperature,
@@ -94,25 +114,28 @@ class ImageApproximationInstance:
         end_time = time.time() + self.max_time
         temperature = initial_temperature
 
-        current_solution = self._generate_initial_solution()
-        print(current_solution)
-        self.visualise_matrix(current_solution)
+        current_solution: _Solution = self._generate_initial_solution()
+        working_solution: _Solution = deepcopy(current_solution)
+        self.visualise_matrix(current_solution.matrix)
 
         while temperature > red_factor and time.time() < end_time:
 
-            neighbour = self._get_random_neighbour(current_solution)
+            neighbour: _Solution = self._get_random_neighbour(working_solution)
+            self.visualise_matrix(neighbour.matrix)
 
             delta_f = \
-                self._compute_mse_of_image_and_other(neighbour) - self._compute_mse_of_image_and_other(current_solution)
+                self._compute_mse_of_image_and_other(neighbour.matrix) - self._compute_mse_of_image_and_other(
+                    current_solution.matrix)
+            print('delta = ', delta_f)
 
             if self._probability(delta_f, temperature, c) > np.random.rand():
-                current_solution = neighbour
+                current_solution = deepcopy(neighbour)
             temperature = temperature * (1 - red_factor)
 
-        return current_solution, self._compute_mse_of_image_and_other(current_solution)
+        return current_solution, self._compute_mse_of_image_and_other(current_solution.matrix)
 
     @staticmethod
-    def visualise_matrix(matrix):
+    def visualise_matrix(matrix: np.ndarray):
         plt.imshow(matrix)
         plt.gray()
         plt.show()
@@ -124,8 +147,16 @@ class ImageApproximationInstance:
 if __name__ == '__main__':
     problemInstance = ImageApproximationInstance.from_file_input([0, 32, 64, 128, 160, 192, 223, 255])
     init_ = problemInstance._generate_initial_solution()
-    problemInstance.visualise_matrix(init_)
-    print(problemInstance._compute_mse_of_image_and_other(init_))
+    print(init_.matrix.shape)
+    print(init_.blocks)
+    # problemInstance.visualise_matrix(init_.matrix)
+    # problemInstance.visualise_matrix(problemInstance._get_random_neighbour(init_).matrix)
+    # problemInstance.visualise_matrix(problemInstance.matrix)
+    print('MSE for initial solution = ', problemInstance._compute_mse_of_image_and_other(init_.matrix))
+
+    sol, val = problemInstance.simulated_annealing(1000, 0.05)
+    problemInstance.visualise_matrix(sol.matrix)
+    print(f'Solution value = {val}')
     # problemInstance.visualise()
     # print(problemInstance.compute_mse(np.array([[1, 1, 1],
     #                                       [2, 2, 2]]), np.array([[0, 0, 0],
