95cc47674ea9df5a7cb22cdfd88fe8a3fd6009e0
Kamil <kamil55525@gmail.com>
2020-04-17 19:27:06 +0200

    Extend _Block class with new features, change some coefficients

--- list2/task2/task2.py
+++ list2/task2/task2.py
@@ -1,17 +1,137 @@
 import fileinput
+import sys
 import time
 from copy import deepcopy
 from dataclasses import make_dataclass
-from typing import Tuple, List
+from typing import Tuple, List, Set
+from itertools import zip_longest
 
 import numpy as np
 from matplotlib import pyplot as plt
 
-_Block = make_dataclass('Block', ['x_start', 'y_start', 'x_length', 'y_length', 'value_inside'])
+
+class _Block:
+    DIRECTIONS = {
+        'U': (0, 1),
+        'R': (1, 0),
+        'D': (0, -1),
+        'L': (-1, 0),
+    }
+
+    def __init__(self, x_start, y_start, x_length, y_length, value_inside):
+        self.x_start = x_start
+        self.y_start = y_start
+        self.x_length = x_length
+        self.y_length = y_length
+        self.value_inside = value_inside
+
+    def __hash__(self):
+        return hash((self.x_start, self.y_start))
+
+    def contains(self, x, y):
+        return self.x_start <= x <= self.x_start + self.x_length and \
+               self.y_start <= y <= self.y_start + self.y_length
+
+    def neighbours_in_direction(self, direction, other_blocks: List['_Block']):
+        neighbours = set()
+        if direction[0] == 0:  # U, D
+            for x in range(self.x_start, self.x_start + self.x_length, 1):
+                y = self.y_start
+                if direction[1] == -1:
+                    y = y + self.y_length
+                for b in other_blocks:
+                    if b.contains(x, y + direction[1]):
+                        neighbours.add(b)
+                        break
+
+        elif direction[1] == 0:  # L, R
+            for y in range(self.y_start, self.y_start + self.y_length, 1):
+                x = self.x_start
+                if direction[0] == 1:
+                    x = x + self.x_length
+                for b in other_blocks:
+                    if b.contains(x + direction[0], y):
+                        neighbours.add(b)
+                        break
+
+        return neighbours
+
+    def get_neighbours(self, other_blocks, max_number_of_neighbours_in_one_direction=np.inf):
+        neighbours = set()
+        for d in self.DIRECTIONS:
+            new_neighbours = self.neighbours_in_direction(d, other_blocks)
+            if len(new_neighbours) <= max_number_of_neighbours_in_one_direction:
+                neighbours.update(new_neighbours)
+        return neighbours
+
+    def can_expand_in_direction(self, direction, other_blocks, min_block_x_size, min_block_y_size):
+        neighbours_in_direction = self.neighbours_in_direction(direction, other_blocks)
+        if len(neighbours_in_direction) != 1:
+            return False
+        neighbour = neighbours_in_direction.pop()
+        return neighbour.y_length > min_block_y_size and neighbour.x_length > min_block_x_size
+
+    """ Returns neighbour in the given direction which can be merged with self-block.
+        If there is no possible candidate for merge, the method returns None.
+    """
+    def can_merge_in_direction(self, direction, blocks):
+        neighbours_on_direction = self.neighbours_in_direction(direction, blocks)
+        if len(neighbours_on_direction) != 1:
+            return None
+        neighbour = neighbours_on_direction.pop()
+        if neighbour.value_inside != self.value_inside:
+            return None
+        return neighbour
+
+    """ Merges given 'other' block with self-block if possible. 
+        Returns bool value telling if merge was performed. Modifies 'other_blocks' collection.
+    """
+    def _merge_in_direction(self, direction, other_blocks: Set['_Block']):
+        other = self.can_merge_in_direction(direction, other_blocks)
+        if other is None:
+            return False
+        up_or_down, left_or_right = direction[0] == 0, direction[1] == 0
+
+        # specific situations and error handling
+        if up_or_down and left_or_right:
+            raise ValueError('Merging in both directions at once is not supported')
+        if not up_or_down and not left_or_right:
+            print(f'Warning: Cannot merge in (0,0) direction', file=sys.stderr)
+            return False
+
+        other_blocks.remove(other)
+        # merging itself
+        if up_or_down:
+            if direction[1] == -1: #  down
+                self.y_length += other.y_length
+            elif direction[1] == 1: #  up
+                self.y_length += other.y_length
+                self.x_start = other.x_start
+                self.y_start = other.y_start
+        elif left_or_right:
+            if direction[0] == -1: #  left
+                self.x_length += other.x_length
+                self.x_start = other.x_start
+                self.y_start = other.y_start
+            elif direction[0] == 1: #  right
+                self.x_length += other.x_length
+        return True
+
+
+    # def merge_with_same_value_blocks(self, blocks: Set['_Block']):
+    #     cannot_merge_more = False
+    #     while not cannot_merge_more:
+    #         cannot_merge_more = True
+    #         for d in self.DIRECTIONS:
+
+
+
 
 
 class _Solution:
-    def __init__(self, image_matrix, blocks: List[_Block]):
+    def __init__(self, image_matrix, blocks: List[_Block], x_free, y_free):
+        self.x_free = x_free
+        self.y_free = y_free
         self.blocks: List[_Block] = blocks
         self.matrix = image_matrix
 
@@ -105,12 +225,12 @@ class ImageApproximationInstance:
                 working_matrix[i:row_end_range, j:column_end_range] = \
                     self._convert_value_to_closest_target_value(mean_value_in_submatrix)
                 blocks_.append(_Block(i, j, row_end_range - i, column_end_range - j, mean_value_in_submatrix))
-        return _Solution(working_matrix, blocks_)
+        return _Solution(working_matrix, blocks_, self.rows % k, self.columns % k)
 
     def simulated_annealing(self,
                             initial_temperature,
                             red_factor,
-                            c=-1):
+                            c=1):
         end_time = time.time() + self.max_time
         temperature = initial_temperature
 
@@ -121,14 +241,15 @@ class ImageApproximationInstance:
         while temperature > red_factor and time.time() < end_time:
 
             neighbour: _Solution = self._get_random_neighbour(working_solution)
-            self.visualise_matrix(neighbour.matrix)
+            # self.visualise_matrix(neighbour.matrix)
 
             delta_f = \
                 self._compute_mse_of_image_and_other(neighbour.matrix) - self._compute_mse_of_image_and_other(
                     current_solution.matrix)
-            print('delta = ', delta_f)
+            print('delta = ', delta_f, f'probability = {self._probability(delta_f, temperature, c)}')
 
             if self._probability(delta_f, temperature, c) > np.random.rand():
+                self.visualise_matrix(neighbour.matrix)
                 current_solution = deepcopy(neighbour)
             temperature = temperature * (1 - red_factor)
 
@@ -154,7 +275,7 @@ if __name__ == '__main__':
     # problemInstance.visualise_matrix(problemInstance.matrix)
     print('MSE for initial solution = ', problemInstance._compute_mse_of_image_and_other(init_.matrix))
 
-    sol, val = problemInstance.simulated_annealing(1000, 0.05)
+    sol, val = problemInstance.simulated_annealing(100, 0.0005)
     problemInstance.visualise_matrix(sol.matrix)
     print(f'Solution value = {val}')
     # problemInstance.visualise()
