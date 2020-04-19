767cd447f74ece4399fa0d12f173da793fd0b6d5
Kamil <kamil55525@gmail.com>
2020-04-19 21:37:18 +0200

    Add test for extending blocks

--- list2/task2/test_block_in_space.py
+++ list2/task2/test_block_in_space.py
@@ -1,3 +1,4 @@
+from copy import copy
 from unittest import TestCase
 from .task2 import _BlockInSpace
 
@@ -47,20 +48,36 @@ class TestBlockInSpace(TestCase):
                 self.assertNotIn(bl, neighbours)
 
     def test_can_expand_in_direction(self):
-        self.assertTrue(self.block.can_expand_in_direction(self.block.DIRECTIONS['D'], 20, 20))
-        self.assertTrue(self.block.can_expand_in_direction(self.block.DIRECTIONS['D'], 100, 20))
-        self.assertFalse(self.block.can_expand_in_direction(self.block.DIRECTIONS['D'], 20, 50))
-        self.assertFalse(self.block.can_expand_in_direction(self.block.DIRECTIONS['D'], 100, 50))
+        result, candidate = self.block.can_expand_in_direction(self.block.DIRECTIONS['D'], 20, 20)
+        self.assertTrue(result)
+        self.assertEqual(candidate, self.on_bottom)
+        result, candidate = self.block.can_expand_in_direction(self.block.DIRECTIONS['D'], 100, 20)
+        self.assertTrue(result)
+        self.assertEqual(candidate, self.on_bottom)
+        result, _ = self.block.can_expand_in_direction(self.block.DIRECTIONS['D'], 20, 50)
+        self.assertFalse(result)
+        result, _ = self.block.can_expand_in_direction(self.block.DIRECTIONS['D'], 100, 50)
+        self.assertFalse(result)
 
-        self.assertTrue(self.block.can_expand_in_direction(self.block.DIRECTIONS['L'], 20, 20))
-        self.assertTrue(self.block.can_expand_in_direction(self.block.DIRECTIONS['L'], 20, 200))
-        self.assertFalse(self.block.can_expand_in_direction(self.block.DIRECTIONS['L'], 50, 20))
-        self.assertFalse(self.block.can_expand_in_direction(self.block.DIRECTIONS['L'], 50, 200))
+        result, candidate = self.block.can_expand_in_direction(self.block.DIRECTIONS['L'], 20, 20)
+        self.assertTrue(result)
+        self.assertEqual(candidate, self.on_left)
+        result, candidate = self.block.can_expand_in_direction(self.block.DIRECTIONS['L'], 20, 200)
+        self.assertTrue(result)
+        self.assertEqual(candidate, self.on_left)
+        result, _ = self.block.can_expand_in_direction(self.block.DIRECTIONS['L'], 50, 20)
+        self.assertFalse(result)
+        result, _ = self.block.can_expand_in_direction(self.block.DIRECTIONS['L'], 50, 200)
+        self.assertFalse(result)
 
-        self.assertFalse(self.block.can_expand_in_direction(self.block.DIRECTIONS['R'], 20, 20))
-        self.assertFalse(self.block.can_expand_in_direction(self.block.DIRECTIONS['R'], 20, 150))
-        self.assertFalse(self.block.can_expand_in_direction(self.block.DIRECTIONS['R'], 100, 20))
-        self.assertFalse(self.block.can_expand_in_direction(self.block.DIRECTIONS['R'], 100, 150))
+        result, _ = self.block.can_expand_in_direction(self.block.DIRECTIONS['R'], 20, 20)
+        self.assertFalse(result)
+        result, _ = self.block.can_expand_in_direction(self.block.DIRECTIONS['R'], 20, 150)
+        self.assertFalse(result)
+        result, _ = self.block.can_expand_in_direction(self.block.DIRECTIONS['R'], 100, 20)
+        self.assertFalse(result)
+        result, _ = self.block.can_expand_in_direction(self.block.DIRECTIONS['R'], 100, 150)
+        self.assertFalse(result)
 
     def test_can_merge_in_direction(self):
         self.assertTrue(self.block.can_merge_in_direction(self.block.DIRECTIONS['D']))
@@ -68,7 +85,6 @@ class TestBlockInSpace(TestCase):
         self.assertTrue(self.block.can_merge_in_direction(self.block.DIRECTIONS['L']))
 
         self.assertFalse(self.block.can_merge_in_direction(self.block.DIRECTIONS['R']))
-
 
     def test_merge_in_direction(self):
         blocks_size = len(self.blocks)
@@ -78,3 +94,22 @@ class TestBlockInSpace(TestCase):
         is_successful = self.block.merge_in_direction(self.block.DIRECTIONS['L'])
         self.assertFalse(is_successful)
         self.assertEqual(blocks_size - 1, len(self.block.space_of_blocks))
+
+    def test_extend_towards_neighbour(self):
+        is_successful = self.block.extend_towards_neighbour(self.on_left, (1, 1), 10)
+        self.assertFalse(is_successful)
+        old_block = copy(self.block)
+        old_left = copy(self.on_left)
+        is_successful = self.block.extend_towards_neighbour(self.on_left, self.block.DIRECTIONS['L'], 2)
+        self.assertTrue(is_successful)
+
+        # y did not change
+        self.assertEqual(self.block.y_length, old_block.y_length)
+        self.assertEqual(self.block.y_start, old_block.y_start)
+        self.assertEqual(self.on_left.y_length, old_left.y_length)
+        self.assertEqual(self.on_left.y_start, old_left.y_start)
+        # x changed
+        self.assertEqual(self.block.x_length, old_block.x_length + 2)
+        self.assertEqual(self.block.x_start, old_block.x_start - 2)
+        self.assertEqual(self.on_left.x_length, old_left.x_length - 2)
+        self.assertEqual(self.on_left.x_start, old_left.x_start)
