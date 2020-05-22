49da084277723f1daf0e50d5cf5a0fddc04b53cd
Kamil <kamil55525@gmail.com>
2020-05-22 23:49:33 +0200

    Update utils

--- list3/utils/utils.py
+++ list3/utils/utils.py
@@ -1,3 +1,4 @@
+from itertools import repeat, starmap
 from typing import Generator, Tuple, TypeVar, Iterable
 
 T = TypeVar('T')
@@ -24,3 +25,28 @@ def flatten(collection):
                 yield x
         else:
             yield element
+
+
+def zip_longest(*args, fill_values_generator, generator_args=None):
+    """
+    Implementation of zim_longest from itertools but with ability to provide function generating fill values.
+    """
+    if generator_args is None:
+        generator_args = []
+    iterators = [iter(it) for it in args]
+    num_active = len(iterators)
+    if not num_active:
+        return
+    while True:
+        values = []
+        for i, it in enumerate(iterators):
+            try:
+                value = next(it)
+            except StopIteration:
+                num_active -= 1
+                if not num_active:
+                    return
+                iterators[i] = starmap(fill_values_generator, repeat(generator_args))
+                value = fill_values_generator(*generator_args)
+            values.append(value)
+        yield tuple(values)
